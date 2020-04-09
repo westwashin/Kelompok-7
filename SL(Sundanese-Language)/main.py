f042dc836098fde04db7d5b9972654dbf3a147af
westwashin <61764924+westwashin@users.noreply.github.com>
2020-04-10 01:05:50 +0700

    Update main.py

--- SL(Sundanese-Language)/main.py
+++ SL(Sundanese-Language)/main.py
@@ -7,9 +7,3 @@ from sys import *
 lexer = sls_lexer.BasicLexer()
 parser = sls_parser.BasicParser()
 env = {}
-
-file = open(argv[1])
-text = file.readlines()
-for line in text:
-    tree = parser.parse(lexer.tokenize(line))
-    sls_interpreter.BasicExecute(tree, env)
